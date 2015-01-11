77cc01fd93817371ba2993c4e3657bf11c4ec667
Phil Friedman <philipmfriedman@gmail.com>
2015-01-10 21:56:22 -0500

    persist to disk

--- Repository.js
+++ Repository.js
@@ -2,7 +2,11 @@
  * Created by PMFriedman on 1/4/2015.
  */
 "use strict";
-var Enumerable = require('linq');
+var Enumerable = require('linq')
+  , storage = require('node-persist')
+  ;
+
+storage.init();
 
 function Repository() {};
 
@@ -10,16 +14,36 @@ Repository.prototype = {
 
   allUsers: [],
 
+  getAllUsers: function(callback) {
+    var users = storage.getItem('users');
+    callback(null, users);
+  },
+
+  saveAllUsers: function(users, callback) {
+    storage.setItem('users', users, callback);
+  },
+
   getUserById: function(id, callback) {
     var err = null;
-    var user = Enumerable.from(Repository.prototype.allUsers).first(function(u) { return u.id == id });
-    callback(err, user);
+    Repository.prototype.getAllUsers(function(err, users) {
+      var user = Enumerable.from(Repository.prototype.allUsers).first(function(u) { return u.id == id });
+      callback(err, user);
+    });
+    //var user = Enumerable.from(Repository.prototype.allUsers).first(function(u) { return u.id == id });
+    //callback(err, user);
   },
 
   addOrUpdateUser: function(user, callback) {
     var err = null;
     Repository.prototype.allUsers.push(user);
-    callback(err, user);
+    Repository.prototype.getAllUsers(function(err, users) {
+      users = users || [];
+      users.push(user);
+      Repository.prototype.saveAllUsers(users, function(err) {
+        callback(err, user);
+      });
+    });
+    //callback(err, user);
   }
 
 }
